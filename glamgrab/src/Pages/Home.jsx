e63a6d07d5af56242afbe85b4fc760c3bcd03d42
Hemandh <hemandh007bond@gmail.com>
2023-02-22 23:14:34 +0530

    day1 prograss

---
+++ glamgrab/src/Pages/Home.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import home from '../Styles/home.css'
+
+const Home = () => {
+  return (
+    <div className='parent'>
+        <div className='banner'>
+            <div>
+            <h1>CHECK MODESENS BEFORE YOU BUY</h1>
+            <h1>Get three months of silver membership for you and your friend.</h1>
+            <button>INVITE FRIEND</button>
+            </div>
+        </div>
+      <div className='sec1'>
+        <div>
+            <div>
+            <h1>Compare 500+ Stores In <span>One</span> Place</h1>
+            <h4>ModeSens allows you to compare prices and availability across hundreds of stores and thousands of brands.</h4>
+            <p>TRY IT OUT</p>
+            </div>
+        </div>
+        <div>
+            <img src="https://cdn.modesens.com/static/img/20221231bg1_en.png" alt="" />
+        </div>
+      </div>
+      <div className='sec3'>
+        <h1>500+ PREMIUM PARTNERS</h1>
+        <div className='sec3para'>
+            <h3>We only partner with the most reputable stores and brands to offer you the largest selections and most trustworthy shopping experience online.</h3>
+        </div>
+        <div className='sec3img'>
+           <div><img src="https://cdn.modesens.com/banner/20220708-SSENSE-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-NET-A-PORTER-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-FARFETCH-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-NORDSTROM-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-MATCHESFASHION-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-SAKS-FIFTH-AVENUE-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-MYTHERESA-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-SHOPBOP-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-ITALIST-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-CETTIRE-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-LUISAVIAROMA-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20220708-BROWNSFASHION-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-GUCCI-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-PRADA-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-SAINT-LAURENT-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-BOTTEGA-VENETA-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-BURBERRY-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-FENDI-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-VERSACE-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-BALENCIAGA-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-GIVENCHY-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-LOEWE-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-OFF-WHITE-logo.png" alt="" /></div>
+           <div><img src="https://cdn.modesens.com/banner/20211014-VALENTINO-logo.png" alt="" /></div>
+        </div>
+      </div>
+      <div className='sec1'>
+        <div>
+            <div>
+            <h1>Locate Any Product In <span>One</span> Second</h1>
+            <h4>Simply copy any product url and search it on ModeSens. Our patented technology helps you easily locate it on ModeSens and compare across 500+ partner stores.</h4>
+            <p>TRY IT OUT</p>
+            </div>
+        </div>
+        <div>
+            <img src="https://cdn.modesens.com/static/img/20221231bg2_en.png" alt="" />
+        </div>
+      </div>
+      <div className='sec3'>
+        <h1>COMPARE AND SHOP WHEREVER YOU GO</h1>
+        <div className='sec3para'>
+            <h3>To unlock all of ModeSens' features and functionality, simply download our App and Browser Extension.</h3>
+            </div>
+        </div>
+        <div className='sec1'>
+        <div>
+            <div>
+            <h1>Install the ModeSens Browser Extension</h1>
+            <h4>Automatically compare products across 500+ stores while you browse at any of our partner stores’ websites.</h4>
+            <button>INSTALL NOW</button>
+            </div>
+        </div>
+        <div>
+            <img src="https://cdn.modesens.com/static/img/20221231bg3_en.png" alt="" />
+        </div>
+      </div>
+    
+    
+    
+    </div>
+)
+}
+
+export default Home
